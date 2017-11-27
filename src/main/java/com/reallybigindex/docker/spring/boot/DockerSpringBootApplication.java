349b3103c6c8a00d57deaea520e90f3c48aaf011
Sushil Verma <sushil@Sushils-MacBook-Pro.local>
2017-11-27 11:05:11 +0530

    Adding ssl support

--- src/main/java/com/reallybigindex/docker/spring/boot/DockerSpringBootApplication.java
+++ src/main/java/com/reallybigindex/docker/spring/boot/DockerSpringBootApplication.java
@@ -1,12 +1,53 @@
 package com.reallybigindex.docker.spring.boot;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @SpringBootApplication
 public class DockerSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerSpringBootApplication.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+		return tomcat;
+	}
+
+	private Connector initiateHttpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(5000);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
 	}
 }
--- src/main/java/com/reallybigindex/docker/spring/boot/controller/HelloController.java
+++ src/main/java/com/reallybigindex/docker/spring/boot/controller/HelloController.java
@@ -1,15 +1,25 @@
 package com.reallybigindex.docker.spring.boot.controller;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
 
 	@GetMapping
-	public String getUser() {
-		return "Hello ReallyBigIndex!";
+	@RequestMapping("/")
+	public String getWelcome() {
+		System.out.println("***** GET / API at:" + new Date());
+		return "Welcome!";
+	}
+	
+	@GetMapping
+	@RequestMapping("/hello")
+	public String getHello() {
+		System.out.println("***** GET /hello API at:" + new Date());
+		return "Hello there!";
 	}
 }
